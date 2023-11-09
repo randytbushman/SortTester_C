fb43bdec9b5e9bcd58c7fe65ae31f321066ce903
randy <randytbushman@gmail.com>
2023-11-09 10:29:18 -0500

    Figure updates

--- figure_generator.py
+++ figure_generator.py
@@ -4,21 +4,7 @@ import os
 import csv
 from cycler import cycler
 import matplotlib.ticker as ticker
-
 from matplotlib.ticker import ScalarFormatter
-
-markers = ['v', 's', "h", 'D', 'X', 'v', 's', "h", 'D', 'X']
-colors = [
-    "C0",
-    "C1",
-    "C3",
-    "C5",
-    "C4",
-    "orangered",
-    "#070600",
-    "#23B5D3",
-    "#700548",
-]
 
 
 # This will be your new function to set up the graph for a subplot
@@ -27,35 +13,47 @@ def setup_subplot(ax, x_exponential, y_exponential, x_lim=None, y_lim=None):
     x_label = f"($10^{x_exponential}$)" if x_exponential else ""
     ax.set_xlabel(f"Array Length {x_label}")
     ax.set_ylabel(f"Computations ($10^{y_exponential}$)")
-
     if x_lim is not None:
         ax.set_xlim(x_lim)
-
     if y_lim is not None:
         ax.set_ylim(y_lim)
 
 
-def generate_subplot_figure(ax, csv_file, x_exponential, y_exponential, x_lim, y_lim, color_offset=0):
+def generate_subplot_figure(ax, csv_file, x_exponential, y_exponential, x_lim, y_lim, linestyles=None, colors=None):
     x_arr, sort_col_list, headers = parse_csv(csv_file)
+    if linestyles is None:
+        linestyles = ['-'] * len(headers)
+    if colors is None:
+        colors = [f"C{i}" for i in range(len(headers))]
     x_arr = [v / (1 * 10 ** x_exponential) for v in x_arr]
     for i in range(1, len(headers)):
         y_arr = [v / (1 * 10 ** y_exponential) for v in sort_col_list[i]]
-        ax.plot(x_arr, y_arr, label=headers[i], color=colors[i - 1 + color_offset]) #f"C{i - 1 + color_offset}")
+        ax.plot(x_arr, y_arr, label=headers[i], linestyle=linestyles[i-1], color=colors[i - 1])
     setup_subplot(ax, x_exponential, y_exponential, x_lim, y_lim)
+
+
+def generate_bar_plot(ax, csv_file, colors):
+    x_arr, sort_col_list, headers = parse_csv(csv_file)
+
+    y_values = [y_arr[-1] for y_arr in sort_col_list]
+    # Check if the number of colors matches the number of headers
+
+    ax.set_xticklabels(headers[1:], rotation=45, ha='center')  # 'ha' is short for horizontalalignment
+
+    # Plotting the bar graph on the given ax object with the provided colors
+    ax.bar(headers[1:], y_values[1:], color=colors)
+
 
 
 def parse_csv(filename):
     with open(filename, 'r') as f:
-
         lines = f.readlines()
         headers = lines[0].strip().split(",")
         sort_col_list = [[] for _ in range(len(headers))]
-
         for row in range(1, len(lines)):
             line = lines[row].strip().split(",")
             for col in range(len(line)):
                 sort_col_list[col].append(float(line[col]))
-
         return sort_col_list[0], sort_col_list, headers
 
 
@@ -72,16 +70,25 @@ def main() -> None:
     fig_7, ((ax6, ax7), (ax8, ax9)) = plt.subplots(2, 2, figsize=(14, 10))
 
     # You would call the appropriate figure generation function for each subplot
-    generate_subplot_figure(ax1, "fig_5.csv", 3, 9, (0, 1000), (0, 180))
-    generate_subplot_figure(ax2, "fig_new.csv", 3, 9, (0, 1000), (0, 180))
-    generate_subplot_figure(ax3, "fig_6.csv", 3, 9, (0, 1000), (0, 180))
-    generate_subplot_figure(ax4, "fig_7.csv", 3, 9, (0, 1000), (0, 180))
-    generate_subplot_figure(ax5, "fig_8.csv", 0, 9, (0, 1500), (-1, 20), 3)
+    line_styles = ["-"] * 5
+    generate_subplot_figure(ax1, "fig_5.csv", 3, 9, (0, 1000), (0, 180), line_styles)
+    generate_subplot_figure(ax2, "fig_new.csv", 3, 9, (0, 1000), (0, 180), line_styles)
+    generate_subplot_figure(ax3, "fig_6.csv", 3, 9, (0, 1000), (0, 180), line_styles)
+    generate_subplot_figure(ax4, "fig_7.csv", 3, 9, (0, 1000), (0, 180), line_styles)
+    generate_subplot_figure(ax5, "fig_8.csv", 0, 9, (0, 1500), (-1, 20), line_styles[-2:], colors=["C3", "C4"])
 
-    generate_subplot_figure(ax6, "fig_9-10a.csv", 3, 9, (0, 1000), (0, 175), 4)
-    generate_subplot_figure(ax7, "fig_9-10b.csv", 3, 9, (0, 1000), (0, 175), 4)
-    generate_subplot_figure(ax8, "fig_9-10c.csv", 3, 9, (0, 1000), (0, 175), 4)
-    generate_subplot_figure(ax9, "fig_9-10d.csv", 3, 9, (0, 1000), (0, 175), 4)
+    line_styles = ["-"] * 5
+    colors = ["C4"] + [f"C{i}" for i in range(4)]
+    '''
+    generate_bar_plot(ax6, "fig_9-10a.csv", colors)
+    generate_bar_plot(ax7, "fig_9-10b.csv", colors)
+    generate_bar_plot(ax8, "fig_9-10c.csv", colors)
+    generate_bar_plot(ax9, "fig_9-10d.csv", colors)
+    '''
+    generate_subplot_figure(ax6, "fig_9-10a.csv", 3, 9, (0, 1000), (0, 175), line_styles, colors)
+    generate_subplot_figure(ax7, "fig_9-10b.csv", 3, 9, (0, 1000), (0, 175), line_styles, colors)
+    generate_subplot_figure(ax8, "fig_9-10c.csv", 3, 9, (0, 1000), (0, 175), line_styles, colors)
+    generate_subplot_figure(ax9, "fig_9-10d.csv", 3, 9, (0, 1000), (0, 175), line_styles, colors)
 
     ax1.set_title("A")
     ax2.set_title("B")
@@ -104,7 +111,6 @@ def main() -> None:
     add_text_to_ax(ax8, "k = 5,000,000", .03)
     add_text_to_ax(ax9, "k = 50,000,000", .03)
 
-
     ax2.legend(loc='upper right')
     ax5.legend(loc='upper right')
     ax7.legend(loc='upper right')
@@ -123,14 +129,6 @@ def main() -> None:
     fig_5.show()
     fig_6.show()
     fig_7.show()
-
-    '''
-    generate_figure_5()
-    generate_figure_6()
-    generate_figure_7()
-    generate_figure_8()
-    #generate_figure_9()
-    #generate_figure_10()'''
 
 
 if __name__ == '__main__':
