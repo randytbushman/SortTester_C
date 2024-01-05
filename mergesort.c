b440cb8dc16ec6ee6810c2b1474cedf8122288c2
randy <randytbushman@gmail.com>
2024-01-04 21:02:24 -0500

    Fixed instruction counter in quicksort

--- mergesort.c
+++ mergesort.c
@@ -1,6 +1,6 @@
 /**
  * @author: Randolph Bushman
- * @date: 11/20/2022
+ * @date: 1/04/2024
  */
 #include "sort.h"
 #include <stdlib.h>
@@ -31,7 +31,8 @@ unsigned long long int merge_sort(int arr[], int arr_length) {
  * @param end_idx the end index where Merge Sort is performed on arr
  */
 void merge_sort_recursive(int arr[], int aux_arr[], int start_idx, int end_idx, unsigned long long int* instruction_counter) {
-    if (start_idx < end_idx) {
+    if (++(*instruction_counter) && start_idx < end_idx) {
+        ++(*instruction_counter);
         int mid = (start_idx + end_idx) >> 1;
         merge_sort_recursive(arr, aux_arr, start_idx, mid, instruction_counter);
         merge_sort_recursive(arr, aux_arr, mid + 1, end_idx, instruction_counter);
@@ -50,25 +51,26 @@ void merge_sort_recursive(int arr[], int aux_arr[], int start_idx, int end_idx, 
 void merge(int arr[], int aux_arr[], int start_idx, int mid_idx, int end_idx, unsigned long long int* instruction_counter) {
     int i = start_idx, j = mid_idx + 1, k = start_idx;
 
-    while ((++instruction_counter && i <= mid_idx) && (++instruction_counter && j <= end_idx)) {
+    while ((++(*instruction_counter) && i <= mid_idx) && (++(*instruction_counter) && j <= end_idx)) {
+        *instruction_counter += 5;  // 3 instructions in `if` statement + 2 instructions in either `if/else` body
         if (arr[i] < arr[j])
             aux_arr[k++] = arr[i++];
         else
             aux_arr[k++] = arr[j++];
     }
 
-    while (++instruction_counter && i <= mid_idx) {
-        instruction_counter += 2;
+    while (++(*instruction_counter) && i <= mid_idx) {
+        *instruction_counter += 2;  // 2 instructions for the array accesses below
         aux_arr[k++] = arr[i++];
     }
 
-    while (++instruction_counter && j <= end_idx) {
-        instruction_counter += 2;
+    while (++(*instruction_counter) && j <= end_idx) {
+        *instruction_counter += 2;  // 2 instructions for the array accesses below
         aux_arr[k++] = arr[j++];
     }
 
-    for (int x = start_idx; ++instruction_counter && x <= end_idx; x++) {
-        instruction_counter += 2;
+    for (int x = start_idx; ++(*instruction_counter) && x <= end_idx; x++) {
+        *instruction_counter += 2;  // 2 instructions for the array accesses below
         arr[x] = aux_arr[x];
     }
 }
